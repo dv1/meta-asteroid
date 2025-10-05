012e1139882d788bcf1cbe54bc60837d8f83df9e
Carlos Rafael Giani <crg7475@mailbox.org>
2025-10-05 23:50:55 +0200

    Auto-select smartwatch OE layer according to the MACHINE name
    Signed-off-by: Carlos Rafael Giani <crg7475@mailbox.org>

--- setup-environment.d/asteroidos.py
+++ setup-environment.d/asteroidos.py
@@ -2,9 +2,34 @@ def __set_defaults_asteroidos_yocto():
 	set_default('DISTRO', 'asteroid')
 
 def __after_init_asteroidos_yocto():
-	PLATFORM_ROOT_DIR = os.environ['PLATFORM_ROOT_DIR']
+	platform_root_dir = os.environ['PLATFORM_ROOT_DIR']
 
-	append_layers([ os.path.join(PLATFORM_ROOT_DIR, 'sources', p) for p in
+	# Prefer the MACHINE from the environment as the smartwatch to use.
+	# When setup-environment is run for the first time, it is normal
+	# that that env variable is set. When it was previously already run
+	# though, and conf/local.conf already exists, often, the MACHINE
+	# env variable is _not_ set. In that case, resort to reading its
+	# value from LOCAL_CONF (which contains the conf/local.conf content).
+	smartwatch = None
+	try:
+		smartwatch = os.environ['MACHINE']
+	except KeyError:
+		for assignment in LOCAL_CONF.conf_data:
+			if assignment[0] == 'MACHINE':
+				try:
+					smartwatch = assignment[2][0]
+				except IndexError:
+					pass
+				except TypeError:
+					pass
+
+	if smartwatch == None:
+		sys.stderr.write('ERROR: Could not determine smartwatch to build for.')
+		sys.exit(1)
+
+	sys.stderr.write('Will build for smartwatch "{}"\n'.format(smartwatch))
+
+	append_layers([ os.path.join(platform_root_dir, 'sources', p) for p in
 		[
 			'meta-openembedded/meta-oe',
 			'meta-openembedded/meta-multimedia',
@@ -16,7 +41,7 @@ def __after_init_asteroidos_yocto():
 			'meta-qt5',
 			'meta-asteroid',
 			'meta-asteroid-community',
-			'meta-smartwatch/meta-dory',
+			'meta-smartwatch/meta-' + smartwatch,
 		]
 	])
 
